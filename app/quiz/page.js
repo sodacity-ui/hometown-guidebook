b2b4d72256509dfa4435f3d17b7eed889834b042
sodacity-ui <ewitteborg@gmail.com>
2026-08-19 10:54:45 -0400

    Add Hometown Match page

---
+++ app/quiz/page.js
@@ -0,0 +1,5 @@
+import QuizClient from './QuizClient';
+
+export const metadata={title:'Hometown Match',description:'Match your priorities to South Carolina communities.'};
+
+export default function QuizPage(){return <main><section className="section alt"><div className="container"><QuizClient/></div></section></main>}
